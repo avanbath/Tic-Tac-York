221fb6da2c595a7db690c56e8b3516a81a9a381e
JustinBlasetti9 <92050664+JustinBlasetti9@users.noreply.github.com>
2021-11-26 12:09:32 -0500

    Start of gameboard creation
    Added image tile (3x3) to the gameboard screen, linked jquery and added css styling to the board using jquery.

--- website/gameBoard/game.js
+++ website/gameBoard/game.js
@@ -1,3 +1,36 @@
-function initializeBoard() {
-    // Initialize game board with empty rows and columns (3x3)
+var start = new Date();
+
+//when page is loaded, create gameboard and start and display a timer
+window.onload= function()
+{
+    initializeBoard();
+    setInterval(function() 
+    {$("#timer").text(parseInt((new Date() - start) / 1000) + "s");}, 1000);
 }
+
+
+function initializeBoard() {
+    // Initialize a counter (for use in ids)
+    var counter=0;
+    //loops to create a gameboard of 9 tiles
+    for(var i = 1; i<=3; i+=1){
+        $("#gameBoard").append("<br>")
+        for(var j=0; j<=2; j+=1){
+            counter+=1;
+            //new var to create each button
+            var newInput= document.createElement("input");
+            //make each button an image
+            $(newInput).attr("type","image");
+            $(newInput).attr("src", "../images/boardTile.png" );
+            //css styling for each button created
+            $(newInput).attr("id","space"+counter);
+            $(newInput).css("margin", "5px");
+            $(newInput).css("width", "180px");
+            $(newInput).css("height", "180px");
+            //display each button to the screen
+            $("#gameBoard").append(newInput);
+
+        }
+    }
+}
+
